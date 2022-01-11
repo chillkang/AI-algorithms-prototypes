4a86682be151e12e378cfbb2efeef9e0e5636075
Seulkee Kang <seulkeekang@gmail.com>
2022-01-11 21:25:59 +0000

    if statements for the game

--- rock_scissor_paper.py
+++ rock_scissor_paper.py
@@ -8,11 +8,29 @@ Created on Tue Jan 11 20:21:58 2022
 
 import random
 
-print ("This is a Rock, Scissors, Paper game. Type 'Rock', 'Scissors', or 'Paper' to play the game, and 'end' to finish the it!")
+print ("This is a 'Rock, Scissors, Paper' game. \nType 'rock', 'scissors', or 'paper' to play the game, and 'end' to finish it!")
 
 possible_actions = ["rock", "scissors", "paper"]
 computer_action = random.choice(possible_actions)
 
-user_action = input("Enter a choice (rock, scissors, paper): ")
+user_action = input("Enter a choice (rock, scissors, paper):")
+
+if user_action == computer_action:
+    print(f"Both players selected {user_action}. It's a tie!")
+elif user_action == "rock":
+    if computer_action == "scissors":
+        print("Rock smashes scissors! You win!")
+    else:
+        print("Paper covers rock! You lose.")
+elif user_action == "paper":
+    if computer_action == "rock":
+        print("Paper covers rock! You win!")
+    else:
+        print("Scissors cuts paper! You lose.")
+elif user_action == "scissors":
+    if computer_action == "paper":
+        print("Scissors cuts paper! You win!")
+    else:
+        print("Rock smashes scissors! You lose.")
       
 print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
