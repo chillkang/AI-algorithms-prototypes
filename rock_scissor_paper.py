c97b0ead62691291af177eb0a34c898083bf1983
Seulkee Kang <seulkeekang@gmail.com>
2022-01-11 21:12:43 +0000

    import random module

--- rock_scissor_paper.py
+++ rock_scissor_paper.py
@@ -6,3 +6,13 @@ Created on Tue Jan 11 20:21:58 2022
 @author: seulkeekang
 """
 
+import random
+
+print ("This is a Rock, Scissors, Paper game. Type 'Rock', 'Scissors', or 'Paper' to play the game, and 'end' to finish the it!")
+
+possible_actions = ["rock", "scissors", "paper"]
+computer_action = random.choice(possible_actions)
+
+user_action = input("Enter a choice (rock, scissors, paper): ")
+      
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
