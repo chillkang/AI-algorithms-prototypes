55873aea351a0abc2a2dc963ef6e3aa74ce64518
Seulkee Kang <seulkeekang@gmail.com>
2022-01-21 20:06:42 +0000

    added functions

--- minds_of_pets.py
+++ minds_of_pets.py
@@ -10,7 +10,7 @@ dog = {
        'name': 'Aji',
        'weight': 6,
        'photo': 'O o O',
-       'talk': 'baff',
+       'talk': 'baffs',
        'energy': 10
        }
 
@@ -18,7 +18,7 @@ pig = {
        'name': 'Cheese',
        'weight': 5,
        'photo': '^ oo ^',
-       'talk': 'oink',
+       'talk': 'oinks',
        'energy': 5
        }
 
@@ -26,7 +26,7 @@ duck = {
         'name': 'Doo',
         'weight': 4,
         'photo': 'O > O',
-        'talk': 'quack',
+        'talk': 'quacks',
         'energy': 3
         }
 
@@ -41,7 +41,12 @@ def sleep(pet):
     pet['energy'] = pet['energy'] + 10
     
 def wake_up(pet):
-    pet['talk']
+    print(f"My pet, {pet['name']} wakes up and", pet['talk'], "!")
     
 def play(pet):
     pet['energy'] = pet['energy'] - 5
+    
+print(dog['weight'])
+
+wake_up(dog)
+
