378a034f2d71515156c226651c83636086939565
Seulkee Kang <seulkeekang@gmail.com>
2022-01-21 17:20:46 +0000

    created pets

--- minds_of_pets.py
+++ minds_of_pets.py
@@ -6,3 +6,24 @@ Created on Sun Jan 16 21:50:19 2022
 @author: seulkeekang
 """
 
+dog = {
+       'name': 'Aji',
+       'weight': 6,
+       'photo': 'O o O',
+       'talk': 'baff'
+       }
+
+pig = {
+       'name': 'Cheese',
+       'weight': 5,
+       'photo': '^ oo ^',
+       'talk': 'oink'
+       }
+
+duck = {
+        'name': 'Doo',
+        'weight': 4,
+        'photo': 'O > O',
+        'talk': 'quack'
+        }
+
