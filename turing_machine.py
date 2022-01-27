f6f63f9e3eda3643cd5fb06c2d1bf6ec6db80d52
Seulkee Kang <seulkeekang@gmail.com>
2022-01-27 17:48:16 +0000

    transition class

--- turing_machine.py
+++ turing_machine.py
@@ -72,4 +72,12 @@ class State:
     def __init__(self, id, state_type):
         self.id = id
         self.type = state_type
+        
+class Transition:
+    def __init__(self, current_state, current_char, new_state, new_char, direction):
+        self.current_state = current_state
+        self.current_char = current_char
+        self.new_state = new_state
+        self.new_char = new_char
+        self.direction = direction
  
