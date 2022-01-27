b1f16b35347be869fe04e3fe6e31b61edba221c2
Seulkee Kang <seulkeekang@gmail.com>
2022-01-27 17:07:35 +0000

    direction class

---
+++ turing_machine.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 17:05:23 2022
+
+@author: seulkeekang
+"""
+
+from enum import Enum
+ 
+class Direction(Enum):
+    Left = 1
+    Right = 2
+    Neutral = 3
