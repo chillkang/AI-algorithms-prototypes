e2a7c588fb3a42483fad3e7668783002a1b10bd8
Seulkee Kang <seulkeekang@gmail.com>
2022-01-27 17:10:10 +0000

    initialize tape class

--- turing_machine.py
+++ turing_machine.py
@@ -12,3 +12,16 @@ class Direction(Enum):
     Left = 1
     Right = 2
     Neutral = 3
+    
+class Tape:
+    def __init__(self, word, alphabet):
+        self.alphabet = alphabet + "$#"       
+        self.head_position = 0
+        self.__init_tape(word)
+ 
+    def __init_tape(self, word):
+        tape = "$";
+        for char in (c for c in word if c in self.alphabet):
+            tape += char
+        tape += "#";
+        self._tape = list(tape)
