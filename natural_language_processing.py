e67ffe49e65c4ca2e5f5d49733a6cce0580baa0c
Seulkee Kang <seulkeekang@gmail.com>
2022-02-01 22:46:49 +0000

    finding contexts shared by words 'monstrous' and 'very'

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -4,5 +4,9 @@ import nltk
 nltk.download('book')
 
 from nltk.book import *
-text1.concordance("monstrous")
-text2.similar("monstrous")
+#text1.concordance("monstrous")
+
+#text1.similar("monstrous")
+#text2.similar("monstrous")
+
+text2.common_contexts(["monstrous", "very"])
