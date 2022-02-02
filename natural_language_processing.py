323f8c4d62e2b77aa528f0c1b14d82f71c3db442
Seulkee Kang <seulkeekang@gmail.com>
2022-02-02 11:02:35 +0000

    finding word definitions and examples using wordnet

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -3,10 +3,15 @@
 import nltk
 nltk.download('book')
 
-from nltk.book import *
+#from nltk.book import *
 #text1.concordance("monstrous")
 
 #text1.similar("monstrous")
 #text2.similar("monstrous")
 
-text2.common_contexts(["monstrous", "very"])
+#text2.common_contexts(["monstrous", "very"])
+
+from nltk.corpus import wordnet
+syn = wordnet.synsets('pain')
+print(syn[0].definition())
+print(syn[0].examples())
