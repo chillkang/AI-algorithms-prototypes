d8c57a37eaf71d2c4f928e653fe0f39240b299af
Seulkee Kang <seulkeekang@gmail.com>
2022-02-02 11:34:54 +0000

    training a classifier to label new movie reviews computing accuracy on the test set and finding informative classifiers

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -53,17 +53,14 @@ import nltk
 # print(translate['jeter'])
 # =============================================================================
 
-# =============================================================================
-# import random
-# =============================================================================
+import random
+
 from nltk.corpus import movie_reviews
-# =============================================================================
-# documents = [(list(movie_reviews.words(fileid)), category)
-#              for category in movie_reviews.categories()
-#              for fileid in movie_reviews.fileids(category)]
-# 
-# print(random.shuffle(documents))
-# =============================================================================
+documents = [(list(movie_reviews.words(fileid)), category)
+             for category in movie_reviews.categories()
+             for fileid in movie_reviews.fileids(category)]
+
+print(random.shuffle(documents))
 
 all_words = nltk.FreqDist(w.lower() for w in movie_reviews.words())
 word_features = list(all_words)[:2000] 
@@ -75,16 +72,16 @@ def document_features(document):
         features['contains({})'.format(word)] = (word in document_words)
     return features
 
-print(document_features(movie_reviews.words('pos/cv957_8737.txt'))) 
+#print(document_features(movie_reviews.words('pos/cv957_8737.txt'))) 
 
 
+featuresets = [(document_features(d), c) for (d,c) in documents]
+train_set, test_set = featuresets[100:], featuresets[:100]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
 
+print(nltk.classify.accuracy(classifier, test_set))
 
-
-
-
-
-
+classifier.show_most_informative_features(5) 
 
 
 
