6f3d50bf29d0aaace680e3b67567f5f982ecff75
Seulkee Kang <seulkeekang@gmail.com>
2022-02-02 11:29:18 +0000

    defining feature extractor for documents with construction of most 2000 frequent words

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -52,10 +52,53 @@ import nltk
 # print(translate['chien'])
 # print(translate['jeter'])
 # =============================================================================
-import random
-from nltk.corpus import movie_reviews
-documents = [(list(movie_reviews.words(fileid)), category)
-             for category in movie_reviews.categories()
-             for fileid in movie_reviews.fileids(category)]
 
-print(random.shuffle(documents))
+# =============================================================================
+# import random
+# =============================================================================
+from nltk.corpus import movie_reviews
+# =============================================================================
+# documents = [(list(movie_reviews.words(fileid)), category)
+#              for category in movie_reviews.categories()
+#              for fileid in movie_reviews.fileids(category)]
+# 
+# print(random.shuffle(documents))
+# =============================================================================
+
+all_words = nltk.FreqDist(w.lower() for w in movie_reviews.words())
+word_features = list(all_words)[:2000] 
+
+def document_features(document): 
+    document_words = set(document) 
+    features = {}
+    for word in word_features:
+        features['contains({})'.format(word)] = (word in document_words)
+    return features
+
+print(document_features(movie_reviews.words('pos/cv957_8737.txt'))) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
