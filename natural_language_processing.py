70e44399281eb8e67c4ba5d7b39364352549d771
Seulkee Kang <seulkeekang@gmail.com>
2022-02-02 11:14:03 +0000

    using entries method specifying languages and making into dictionaries

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -38,5 +38,13 @@ import nltk
 # =============================================================================
 
 from nltk.corpus import swadesh
-print(swadesh.fileids())
-print(swadesh.words('en'))
+# =============================================================================
+# print(swadesh.fileids())
+# print(swadesh.words('en'))
+# =============================================================================
+
+fr2en = swadesh.entries(['fr', 'en'])
+print(fr2en)
+translate = dict(fr2en)
+print(translate['chien'])
+print(translate['jeter'])
