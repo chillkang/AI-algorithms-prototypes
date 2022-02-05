4763ee4db72785bb424531172aa931f6cf5b94ce
Seulkee Kang <seulkeekang@gmail.com>
2022-02-05 20:27:52 +0000

    sample comments

--- images_and_deep_learning.py
+++ images_and_deep_learning.py
@@ -3,3 +3,9 @@
 import cv2
 import numpy as np
 import matplotlib . pyplot as plt
+
+########## Section 2.X −<insert description>####################### 
+# load image
+#<your code here>
+# display image
+#<your code here> ##############################################
