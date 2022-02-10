d5d52cf1d9b0b828803bb15c3a2f54acdcef9957
Seulkee Kang <seulkeekang@gmail.com>
2022-02-10 23:39:11 +0000

    load and display image using opencv

--- images_and_deep_learning/work_with_image.py
+++ images_and_deep_learning/work_with_image.py
@@ -2,4 +2,14 @@
 
 import cv2
 import numpy as np
-import matplotlib . pyplot as plt
+import matplotlib.pyplot as plt
+
+########## Section 2.a −<Loading and Displaying Images>####################### 
+# load image
+img = cv2.imread('elephant.jpeg')
+
+# display image
+plt.imshow(img)
+plt.show()
+cv2.imwrite('elephant_opencv.png', img)
+##############################################################################
