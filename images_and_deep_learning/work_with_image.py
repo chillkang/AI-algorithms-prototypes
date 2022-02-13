dcd9e5a6c2d73131bcc6bc6d92198b0f377f4068
Seulkee Kang <seulkeekang@gmail.com>
2022-02-13 22:44:25 +0000

    Added comments

--- images_and_deep_learning/work_with_image.py
+++ images_and_deep_learning/work_with_image.py
@@ -6,16 +6,16 @@ import matplotlib.pyplot as plt
 from PIL import Image, ImageChops
 
 
-########## Section 2.a −<Loading and Displaying Images>####################### 
+########## Section 2 −<Loading and Displaying Images>####################### 
 # load image
 img = cv2.imread('elephant.jpeg')
 
-# display and save image
+# display the image using Matplotlib and save image
 plt.imshow(img)
 plt.show()
 cv2.imwrite('elephant_opencv.png', img)
 
-# load image
+# load image converted into a RGB space
 im_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 # display and save image
@@ -23,11 +23,7 @@ plt.imshow(im_rgb)
 plt.show()
 cv2.imwrite('elephant_matplotlib.png', im_rgb)
 
-im_pillow = np.array(Image.open('elephant.jpeg'))
-im_bgr = cv2.cvtColor(im_pillow, cv2.COLOR_RGB2BGR)
-cv2.imwrite('elephant_matplotlib.png', im_bgr)
-
-# load image
+# load image in grayscale
 img_gray = cv2.imread('elephant.jpeg', cv2.IMREAD_GRAYSCALE)
 
 # display and save image
@@ -37,7 +33,7 @@ cv2.imwrite('elephant_gray.png', img_gray)
 ##############################################################################
 
 ########## Section 3 −<Cropping>############################################## 
-# print dimensions and load image
+# print dimensions of image and crop out the small elephant. 
 print(img.shape) 
 cropped_img = im_rgb[400:900, 100:600]
 
@@ -48,102 +44,99 @@ cv2.imwrite('babyelephant.png', cropped_img)
 ##############################################################################
 
 ########## Section 4 -<Pixel-wise Arithmetic Operations>######################
-# Read in the image in color, and convert it to RGB space.
+# read in the image in color, and convert it to RGB space.
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.imshow(img_rgb)
+plt.show()
+pixel= img_rgb[999, 1499]
+print(pixel) # [168 171  94]
 
-# =============================================================================
-# img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-# plt.imshow(img_rgb)
-# plt.show()
-# pixel= img_rgb[999, 1499]
-# print(pixel) # [168 171  94]
-# 
-# img_rgb = img_rgb + 256
-# 
-# print(img_rgb.dtype) #uint16
-# plt.imshow(img_rgb)
-# plt.show() 
-# pixel= img_rgb[999, 1499]
-# print(pixel) #[424 427 350]
-# 
-# img_rgb = img_rgb.astype(np.uint8)
-# print(img_rgb.dtype)  #unit8
-# plt.imshow(img_rgb)
-# plt.show()
-# pixel= img_rgb[999, 1499]
-# print(pixel) #[168 171 94]
-# 
-# #Clipping input data to the valid range for imshow with RGB data ([0..1] 
-# #for floats or [0..255] for integers).
-# #The RGB/BGR values are [0, 255] BECAUSE uint8 has that range. 
-# #The reason why Uint8 is very common is because it is the standard way to display images, 
-# #in which each pixel ranges between 0 and 255.
-# =============================================================================
+# add the value 256 to every single pixel in the image
+img_rgb = img_rgb + 256
 
+# check the image's datatype and the pixel values
+print(img_rgb.dtype) #uint16
+plt.imshow(img_rgb)
+plt.show() 
+pixel= img_rgb[999, 1499]
+print(pixel) #[424 427 350]
 
-# =============================================================================
-# bgr_img = cv2.imread("elephant.jpeg")
-# (b,g,r) = cv2.split(bgr_img)
-# 
-# plt.figure()
-# plt.imshow(b)
-# plt.show()
-# plt.figure()
-# plt.imshow(g)
-# plt.show()
-# plt.figure()
-# plt.imshow(r)
-# plt.show()
-# 
-# b = cv2.add(b, 256)
-# g = cv2.add(g, 256)
-# r = cv2.add(r, 256)
-# 
-# plt.figure()
-# plt.imshow(b)
-# plt.show()
-# plt.figure()
-# plt.imshow(g)
-# plt.show()
-# plt.figure()
-# plt.imshow(r)
-# plt.show()
-# print(b.dtype) #uint8
-# 
-# #If you have a datatype that will not overflow if you add 100 to it (i.e. uint16) 
-# #then you can do simply use numpy.add() to do it:
-# #Most images are of type uint8 though so you'd want to clamp the output to a range of 0 to 255. 
-# #But when you do the add, you'd want to use a datatype that didn't overflow. 
-# #So there is some casting (astype()) needed:
-# 
-# bgr_img = cv2.merge([b, g, r])
-# 
-# plt.figure()
-# plt.imshow(bgr_img)
-# plt.show()
-# cv2.imwrite('merged256.png', bgr_img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-# 
-# =============================================================================
+# convert the image back to uint8 and display the image and the pixel values
+img_rgb = img_rgb.astype(np.uint8)
+print(img_rgb.dtype)  #unit8
+plt.imshow(img_rgb)
+plt.show()
+pixel= img_rgb[999, 1499]
+print(pixel) #[168 171 94]
+
+# split the image into R,G,B channels seperately
+bgr_img = cv2.imread("elephant.jpeg")
+(b,g,r) = cv2.split(bgr_img)
+
+plt.figure()
+plt.imshow(b)
+plt.show()
+plt.figure()
+plt.imshow(g)
+plt.show()
+plt.figure()
+plt.imshow(r)
+plt.show()
+
+# add 256 to each one
+b = cv2.add(b, 256)
+g = cv2.add(g, 256)
+r = cv2.add(r, 256)
+
+plt.figure()
+plt.imshow(b)
+plt.show()
+plt.figure()
+plt.imshow(g)
+plt.show()
+plt.figure()
+plt.imshow(r)
+plt.show()
+print(b.dtype) #uint8
+
+#If you have a datatype that will not overflow if you add 100 to it (i.e. uint16) 
+#then you can do simply use numpy.add() to do it:
+#Most images are of type uint8 though so you'd want to clamp the output to a range of 0 to 255. 
+#But when you do the add, you'd want to use a datatype that didn't overflow. 
+#So there is some casting (astype()) needed:
+
+# merge the channels back together and display the image
+bgr_img = cv2.merge([b, g, r])
+
+plt.figure()
+plt.imshow(bgr_img)
+plt.show()
+cv2.imwrite('merged256.png', bgr_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 ##############################################################################
 
 ########## Section 5 -<Resizing images>#######################################
+# read the image in color and convert it to RGB space
 img = cv2.imread('elephant.jpeg')
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+# downsample the image by 10x in width and height and save the iamge
 height, width = img_rgb.shape[:2]
 downsample_img = cv2.resize(img_rgb, (round(width / 10), round(height / 10)), interpolation=cv2.INTER_AREA)
 plt.imshow(downsample_img)
 plt.show()
 cv2.imwrite('elephant_10xdown.png', downsample_img)
 
-upsized1 = cv2.resize(img_rgb, (round(width * 10), round(height * 10)), interpolation = cv2.INTER_NEAREST)
+
+upsized1 = cv2.resize(downsample_img, (round(width * 10), round(height * 10)), interpolation = cv2.INTER_NEAREST)
 plt.imshow(upsized1)
 plt.show()
 upsized1_BGR = cv2.cvtColor(upsized1, cv2.COLOR_RGB2BGR)
 cv2.imwrite('elephant_10xup_nearst_neighbor.png', upsized1_BGR)
 
-upsized2 = cv2.resize(img_rgb, (round(width * 10), round(height * 10)), interpolation = cv2.INTER_CUBIC)
+upsized2 = cv2.resize(downsample_img, (round(width * 10), round(height * 10)), interpolation = cv2.INTER_CUBIC)
 plt.imshow(upsized2)
 plt.show()
 upsized2_BGR = cv2.cvtColor(upsized2, cv2.COLOR_RGB2BGR)
@@ -166,9 +159,9 @@ diff2 = np.array(diff2)
 cv2.imwrite('elephant_diff_with_bicubic.png', diff2)
 
 sum1 = cv2.sumElems(diff1)
-print(sum1) #(84037224.0, 72856994.0, 62359036.0, 0.0)
+print(sum1) #(83336620.0, 71851852.0, 62169552.0, 0.0)
 sum2 = cv2.sumElems(diff2)
-print(sum2) #(84047843.0, 72868982.0, 62361368.0, 0.0)
+print(sum2) #(83494819.0, 71880764.0, 62107822.0, 0.0)
 
 cv2.waitKey(0)
 
